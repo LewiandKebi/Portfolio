ba7a995d95094e729a72923cd9858412308d7d49
LewiandKebi <etafe1123@gmail.com>
2026-03-29 07:59:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Portfolio loaded!");
+function showMessage() {
+  alert("Hello! Your JavaScript is working 🚀");
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("Message sent successfully! 🚀");
+}
